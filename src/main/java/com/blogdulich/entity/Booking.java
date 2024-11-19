f446254ce58e13edfd8292e57b57c835098c5ca5
DawnMinhHuy <minhhuygo012304@gmail.com>
2024-11-19 18:43:33 +0700

    templates

--- src/main/java/com/blogdulich/entity/Booking.java
+++ src/main/java/com/blogdulich/entity/Booking.java
@@ -1,5 +1,14 @@
 package com.blogdulich.entity;
 import javax.persistence.*;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+
 import java.util.Date;
 
 @Entity
@@ -10,7 +19,7 @@ import java.util.Date;
 public class Booking{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = Generated.IDENTITY)
     @Column(name = "Booking_ID")
     private Long bookingId;
 
